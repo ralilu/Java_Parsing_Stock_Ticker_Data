3622b3f6c6ae43826b6ceaa3d701bc21d3649690
ralilu <58275921+ralilu@users.noreply.github.com>
2022-10-15 21:30:47 -0400

    Update UnzipUtility.java

--- src/java_Parsing_Stock_Ticker_Data/UnzipUtility.java
+++ src/java_Parsing_Stock_Ticker_Data/UnzipUtility.java
@@ -2,11 +2,27 @@ package java_Parsing_Stock_Ticker_Data;
 import java.util.*;
 import java.util.zip.*;
 import java.io.*;
+import java.net.URL;
+import java.nio.*;
+import java.nio.channels.*;
+
+/*
+ * STEP 1: Download zip from the internet (NSE website)
+ * JAVA has url processing classes to download a file from an internet url and save it to a file on local disk 
+ * But, the NSE tries to block automated scraping by programs
+ * So, we need to jump through a few hoops to convince the NSE that its a human downloading the file 
+ * STEP 2: Unzip the zip file and get a csv file
+ * Make sure STEP 1 worked, and the zip file actally exists. Then open the zip file. 
+ * Write code using JAVA libraries to get a list of all files inside this zip file. Use a for loop to iterate through each file within, and extract i.e. unzip it. 
+ * Keep a list of extracted files so we know what we unzipped 
+ * Finally, close the zip file 
+ */
 
 public class UnzipUtility {
 	//we will use this class to abstract the rest of the code from 2 operations
 	//1. downloading the zip from the NSE website
 	//2. unzipping the downloaded from the website to some location on our local machine
+	//(DOWNLOAD, UNZIP, SUMMARIZE)
 	
 	//note the order in which we went about this - we first decided what member functions we would like our class to have. 
 	//We defined the 'interface' for these functions, i.e. what inputs and outputs those functions would have
@@ -19,6 +35,29 @@ public class UnzipUtility {
 	
 	public static List<String> downloadAndUnzip(String urlString, String zipFilePath, String destDirectory)
 	throws IOException {
+		
+		//this method downloads the URL before it can unzip it
+		
+		URL tariff = new URL(urlString);
+		//the NSE will reject an attempt to download this file, unless it has a user-agent property set that makes it look like the request came from a human user 
+		//(not a computer program). 
+		//Let's figure out a user-agent and set it
+		String myUserAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36";
+		
+		java.net.URLConnection c = tariff.openConnection();
+		c.setRequestProperty("User-Agent", myUserAgentString);
+		//we got an object that corresponds to the url connection, got our user agent (by looking it up through google "what is my user agent") and then set the property 
+		//of the url connection
+		
+		//more biolerplate code coming up: - to dowload the url now that we have opened it 
+		ReadableByteChannel zipByteChannel = Channels.newChannel(c.getInputStream());
+		FileOutputStream fos = new FileOutputStream(zipFilePath);
+		fos.getChannel().transferFrom(zipByteChannel, 0, Long.MAX_VALUE);
+		//Basically these 3 lines save the download url to our local hard disk (to the path specified in the 'zipFilePath'
+		//variable) and do the low-level file handling for this to happen
+		
+		//now unzip the file using the functions called below
+		return unzip(zipFilePath, destDirectory);
 		
 	}
 	
@@ -64,6 +103,7 @@ public class UnzipUtility {
 			zipEntry = zipIn.getNextEntry();
 		}
 		
+		return unzippedFileList;
 	}
 	
 	private static String extractFile(ZipInputStream zipIn, String filePath) throws IOException {
